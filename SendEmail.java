9aa9549bb64745508b4a6e6a61f3455248a8c487
Gaurav Rajpoot <gaurav.rajpoot@nagarro.com>
2020-06-09 13:41:16 +0530

    sending mail in outlook using java

---
+++ SendEmail.java
@@ -0,0 +1,81 @@
+package com.nagarro.testing;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class SendEmail {
+
+    public static void main(String[]args) throws IOException {
+
+        final String username = "email@outlook";
+        final String password = "password";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "outlook.office365.com");
+        props.put("mail.smtp.port", "587");
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("gaurav.rajpoot@nagarro.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse("gaurav.rajpoot@nagarro.com"));
+            message.setSubject("Test message");
+            message.setText("Gaurav Rajpoot has send this message");
+
+         // Create the message part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Now set the actual message
+            messageBodyPart.setText("This is message body");
+
+            // Create a multipar message
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Part two is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "C:\\Users\\gauravrajpoot\\Desktop\\Test.txt";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            message.setContent(multipart);
+            // Send message
+            Transport.send(message);
+
+            System.out.println("Sent message successfully....");
+
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
